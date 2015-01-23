13d8d87de1828661aedae55e32f3dcabfd9cb33b
Benjamin <baleeds@ncsu.edu>
2015-01-23 12:21:08 -0500

    Changed routing to include a login page.  Fixed links on controllers for all views.

---
+++ app/flexibox-login/controllers/loginController.js
@@ -0,0 +1,25 @@
+// ------------------------------------------------------------
+// Controller that is loaded on the home view (index).  Has the ability
+// to add and remove projects.
+//-------------------------------------------------------------
+define([
+        '../module',
+        '../namespace',
+        '../../common/namespace'
+    ],
+    function (module, namespace, namespaceCommon) {
+        'use strict';
+
+        var name = namespace + ".loginController";
+        module.controller(name,
+              ['$scope','$rootScope', '$log',
+               function ($scope, $rootScope, $log) {
+                   
+
+
+            if ($rootScope.username != "") {
+                $location.path('/projects');
+            }
+
+        }]);
+    });
