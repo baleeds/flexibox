078cc7fb00206d041dc47579741902a59fef3389
Nolan <njpiland@ncsu.edu>
2015-01-25 11:38:29 -0500

    Adding some more stuff for jenkins

--- karma.conf.js
+++ karma.conf.js
@@ -36,7 +36,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'junit'],
 
 
     // web server port
@@ -63,6 +63,10 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true,
+
+    junitReporter : {
+      outputFile: 'test-results.xml'
+    }
   });
 };
