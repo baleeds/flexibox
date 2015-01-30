ae65964907196a1b3023bf845e71c55ec23498cb
Benjamin <baleeds@ncsu.edu>
2015-01-30 11:33:44 -0500

    Removed loginFactory

--- app/common/factories/_index.js
+++ app/common/factories/_index.js
@@ -4,7 +4,6 @@ define([
         "./setFactory",
         "./postFactory",
         "./utilityFactory",
-        "./loginFactory",
         "./signupFactory",
         "./sessionFactory"
 ], function () {
