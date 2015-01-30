e43813de91ba2c43838bc0fcdadb411577b5b6dc
dfperry2 <dfperry2@ncsu.edu>
2015-01-30 10:49:56 -0500

    Deleted Flash Factory

--- app/common/factories/_index.js
+++ app/common/factories/_index.js
@@ -4,7 +4,6 @@ define([
         "./setFactory",
         "./postFactory",
         "./utilityFactory",
-        "./flashFactory",
         "./loginFactory",
         "./signupFactory",
         "./sessionFactory",
