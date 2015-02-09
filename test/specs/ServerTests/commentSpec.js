466e09a1c83b7c82e25bdb0e07720aff38f10fb1
Nolan <njpiland@ncsu.edu>
2015-02-08 22:28:21 -0500

    Adding more frisby testing and some test data as well

--- test/specs/ServerTests/commentSpec.js
+++ test/specs/ServerTests/commentSpec.js
@@ -1,20 +1,49 @@
 var frisby = require('frisby');
 
 var URL = "http://localhost:8080/";
-var PID = "54cd03edf35c8f0c1d000004";
-var SID = "54cd03f6f35c8f0c1d000005";
-var OID = "54cd4bc97a3e43e41d000005";
+var PID = "54d82057b46e200418000006";
+var SID = "54d82082b46e200418000007";
+var OID = "54d8209cb46e200418000008";
 
 frisby.create("commentRoutes")
     .post("http://localhost:8080/api/login",
     {
-        email: "nolanpiland@gmail.com",
-        password: "Nolan"
+        email: "commenter@test.com",
+        password: "commenter"
     }).after(function(body, res){
         var cookie = res.headers['set-cookie'][0].split(';')[0];
-        frisby.create("GET : api/project/pid/sets/sid/post/id?includeComments=1")
+
+        frisby.create("POST : api/project/pid/sets/sid/post/id")
             .addHeader('Cookie', cookie)
-            .get(URL + "api/projects/" + PID + "/sets/" + SID + "/posts/" + OID + "?includeComments=1")
+            .post(URL + "api/projects/" + PID + "/sets/" + SID + "/posts/" + OID + "/comments",{
+                "posterName":"commenter",
+                "txt":"Adding a comment",
+                "width":10,
+                "height":10,
+                "color":"rgb(101,167,187)",
+                "number":2,
+                "smallest":{
+                    "x":0,
+                    "y":8
+                }
+            })
             .expectStatus(200)
+            .expectJSON({
+                name : "Nolan's Schedule",
+                description : "This is Nolan's Schedule",
+                imageURL : "uploads\\c0c74226fdae8f0a66b4b207d3134625.PNG",
+                "comments":
+                    [{ },{ },{
+                        "posterName":"commenter",
+                        "txt":"Adding a comment",
+                        "width":10,
+                        "height":10,
+                        "color":"rgb(101,167,187)",
+                        "number":2,
+                        "smallest":{
+                            "x":0,
+                            "y":8
+                        }}]
+            })
             .toss();
     }).toss();
---
+++ test/specs/ServerTests/postSpec.js
@@ -0,0 +1,56 @@
+var frisby = require('frisby');
+
+var URL = "http://localhost:8080/";
+var PID = "54d82057b46e200418000006";
+var SID = "54d82082b46e200418000007";
+var OID = "54d8209cb46e200418000008";
+
+frisby.create("commentRoutes")
+    .post("http://localhost:8080/api/login",
+    {
+        email: "commenter@test.com",
+        password: "commenter"
+    }).after(function(body, res){
+        var cookie = res.headers['set-cookie'][0].split(';')[0];
+
+        frisby.create("GET : api/project/pid/sets/sid/post/id?includeComments=1")
+            .addHeader('Cookie', cookie)
+            .get(URL + "api/projects/" + PID + "/sets/" + SID + "/posts/" + OID + "?includeComments=1")
+            .expectStatus(200)
+            .expectJSON({
+                name : "Nolan's Schedule",
+                description : "This is Nolan's Schedule",
+                imageURL : "uploads\\c0c74226fdae8f0a66b4b207d3134625.PNG",
+                "comments":
+                    [{
+                        "posterName":"commenter",
+                        "txt":"Work here",
+                        "width":85,
+                        "height":41,
+                        "color":"rgb(145,127,127)",
+                        "number":1,
+                        "_id":"54d820b5b46e200418000009",
+                        "replies":[{
+                            "posterName":"commenter",
+                            "txt":"No, that should be gym",
+                            "_id":"54d820ceb46e20041800000b"
+                        }],
+                        "smallest":{
+                            "x":505,
+                            "y":108
+                        }},{
+                        "posterName":"commenter",
+                        "txt":"Gym here",
+                        "width":78,
+                        "height":39,
+                        "color":"rgb(177,150,109)",
+                        "number":2,
+                        "_id":"54d820c6b46e20041800000a",
+                        "replies":[],
+                        "smallest":{
+                            "x":295,
+                            "y":106
+                        }}]
+            })
+            .toss();
+    }).toss();
