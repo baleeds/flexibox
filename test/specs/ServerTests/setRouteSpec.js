fadfc91a327d833dc7beca5b401b85178ddd326f
Nolan <njpiland@ncsu.edu>
2015-02-10 21:26:12 -0500

    Adding some set route testing

---
+++ test/specs/ServerTests/setRouteSpec.js
@@ -0,0 +1,47 @@
+var frisby = require('frisby');
+var Utils = require('../../testingUtils');
+
+var URL = "http://localhost:8080/";
+var PID = "54d82057b46e200418000006";
+var SID = "54d82082b46e200418000007";
+
+Utils.loadTestData();
+
+frisby.create("setRoutes")
+    .post("http://localhost:8080/api/login",
+    {
+        email: "commenter@test.com",
+        password: "commenter"
+    }).after(function (body, res) {
+        var cookie = res.headers['set-cookie'][0].split(';')[0];
+
+        frisby.create("GET : api/project/pid/sets")
+            .addHeader('Cookie', cookie)
+            .get(URL + "api/projects/" + PID + "/sets")
+            .expectStatus(200)
+            .expectJSON([{
+                "name": "DB Flexibox Set",
+                "description": "A set containing flexibox stuff.",
+                "entryURL": "/api/projects54d82057b46e200418000006/sets/undefined",
+                "postsURL": "/api/projects54d82057b46e200418000006/sets/undefined/posts",
+                "_id": "54d82082b46e200418000007"
+            }])
+            .toss();
+
+        frisby.create("POST : api/project/pid/sets")
+            .addHeader('Cookie', cookie)
+            .post(URL + "api/projects/" + PID + "/sets", {
+                name: "AMERICA",
+                description : "AMERICA IS THE GREATEST COUNTRY EVER!!!!"
+            })
+            .expectStatus(200)
+            .expectJSON({sets : [{
+                "name": "DB Flexibox Set",
+                "description": "A set containing flexibox stuff."
+            }, {
+                name: "AMERICA",
+                description : "AMERICA IS THE GREATEST COUNTRY EVER!!!!"
+            }]})
+            .toss();
+
+    }).toss();
