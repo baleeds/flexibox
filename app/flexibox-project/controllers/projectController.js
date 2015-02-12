896f9eff2d523c4d5afcdefd35258700bf4b926a
Benjamin <baleeds@ncsu.edu>
2015-02-12 16:10:39 -0500

    Added ability to edit sets names and descriptions

--- app/flexibox-project/controllers/projectController.js
+++ app/flexibox-project/controllers/projectController.js
@@ -11,13 +11,34 @@ define([
 
 		var name = namespace + ".projectController";
 		module.controller(name,
-			['$scope', '$rootScope', '$log', namespaceCommon + '.projectFactory', '$routeParams', namespaceCommon + '.utilityFactory',
-			function ($scope, $rootScope, $log, projectFactory, $routeParams, utilityFactory) {
+			['$scope', '$rootScope', '$log', namespaceCommon + '.projectFactory', '$routeParams', namespaceCommon + '.utilityFactory', namespaceCommon + '.setFactory',
+			function ($scope, $rootScope, $log, projectFactory, $routeParams, utilityFactory, setFactory) {
 	
 			var logger = $log.getInstance(name);      // logger
 			$scope.project = {};                      // Local instance of focused project.  Only contains project name, description, set.name, set.description
 			$scope.formData = {};                     // Data from the new set modal
+			$scope.editableSet = {};
+			var oldSet = {};
 
+			$scope.setEditable = function(setId) {
+				$scope.editableSet = utilityFactory.findById($scope.project.sets, setId);
+				oldSet.name = $scope.editableSet.name;
+				oldSet.description = $scope.editableSet.description;
+			};
+
+			$scope.confirmEdit = function() {
+				setFactory.updateSet($scope.project._id, $scope.editableSet._id, $scope.editableSet)
+					.success(function(set) {
+						//
+					}).error(function(err) {
+						alert("error");
+					});
+			};
+
+			$scope.cancelEdit = function() {
+				$scope.editableSet.name = oldSet.name;
+				$scope.editableSet.description = oldSet.description;
+			};
 
 			// On controller load, populate local instance of focused project
 			projectFactory.getProject($routeParams.projectId)
