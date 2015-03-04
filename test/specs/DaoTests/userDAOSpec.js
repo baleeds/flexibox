09f506c6041ebd9ac940ac0a016dd439d1638fb0
Nolan <njpiland@ncsu.edu>
2015-03-03 19:47:59 -0500

    Adding async support to the DAOs this shouldn't change functionality, but should make our code much more organized.

--- test/specs/DaoTests/userDAOSpec.js
+++ test/specs/DaoTests/userDAOSpec.js
@@ -1,43 +1,42 @@
 var UserDAO = require('../../../server/dao/userDAO');
 var TestUtils = require('../../testingUtils');
+
 var async = require('async');
 
-describe("userDAO Tests", function() {
+describe("UserDAO Tests", function () {
 
     beforeEach(function () {
         TestUtils.loadTestData();
     });
 
-    describe("deleteProjects", function() {
-        it("Deleting a shared project", function() {
-            var searchCallback = function(user, callback) {
-                if(user[0].name == 'owner') {
+    describe("deleteProjects", function () {
+        it("Deleting a shared project", function () {
+            async.waterfall([
+                function (callback) {
+                    UserDAO.deleteProjects("54d82057b46e200418000006", callback);
+                },
+                function (callback) {
+                    UserDAO.userSearch('owner', function (user) {
+                        callback(null, user);
+                    })
+                },
+                function (user, callback) {
+                    expect(user.name).toBe('owner');
                     expect(user[0].projectsVisible.length).toBe(1);
-                } else if (user[0].name == 'commenter') {
+                    callback(null);
+                },
+                function (callback) {
+                    UserDAO.userSearch('commenter', callback)
+                }
+
+            ], function (err, user) {
+                if (err) {
+                    expect(2).toBe(1);
+                } else {
+                    expect(user.name).toBe('commenter');
                     expect(user[0].projectsVisible.length).toBe(0);
                 }
-                callback(null);
-            };
-
-            async.waterfall([
-                function(callback) {
-                    UserDAO.deleteProjects("54d82057b46e200418000006", function(){
-                        callback(null);
-                    });
-                },
-                function(callback){
-                    UserDAO.userSearch('owner', function(user){
-                        callback(null, user);
-                    })
-                },
-                searchCallback,
-                function(callback){
-                    UserDAO.userSearch('commenter', function(user){
-                        callback(null, user);
-                    })
-                },
-                searchCallback
-            ]);
+            });
 
         });
     });
