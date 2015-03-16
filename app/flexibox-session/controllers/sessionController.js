8c0135071fb67d98050afbd6303ccd05f492ae15
dfperry2 <dfperry2@ncsu.edu>
2015-03-16 15:02:05 -0400

    Added a dropdown box.. it really doesn't do anything though. just looks sweet.

--- app/flexibox-session/controllers/sessionController.js
+++ app/flexibox-session/controllers/sessionController.js
@@ -7,37 +7,43 @@ define([
 		'../namespace',
 		'../../common/namespace'
 	],
-function (module, namespace, namespaceCommon) {
-	'use strict';
+	function (module, namespace, namespaceCommon) {
+		'use strict';
 
-	var name = namespace + ".sessionController";
-	module.controller(name, ['$scope','$rootScope', namespaceCommon + '.sessionFactory', '$location', function ($scope,
-																			$rootScope, sessionFactory, $location) {
-	$scope.user = {}; // user that is currently logged in
+		var name = namespace + ".sessionController";
+		module.controller(name, ['$scope','$rootScope', namespaceCommon + '.sessionFactory', '$location', function ($scope,
+																													$rootScope, sessionFactory, $location) {
+			$scope.user = {}; // user that is currently logged in
+			$scope.options = [
+				{ label: 'Project Name', value: 'name' },
+				{ label: 'Project Tags', value: 'tags' },
+				{label:  'Project Owner', value: 'owner'}
+			];
+			$scope.filterModels = [];
 
-	sessionFactory.getCurrentUser()
-		.success(function(user) {
-			$scope.user = user;
-		})
-		.error(function(err) {
-			// Do nothing
-		});
+			sessionFactory.getCurrentUser()
+				.success(function(user) {
+					$scope.user = user;
+				})
+				.error(function(err) {
+					// Do nothing
+				});
 
-	$rootScope.$on('userChanged', function(event, newUser) {
-		$scope.user = newUser;
+			$rootScope.$on('userChanged', function(event, newUser) {
+				$scope.user = newUser;
+			});
+
+			$scope.fd = {};           // form data from the view
+			$scope.location = $location.path();
+
+
+			// Log out user
+			$scope.logout = function() {
+				sessionFactory.logout();
+				$scope.user = null;
+				sessionFactory.user = null;
+				$location.path('/login');
+			};
+
+		}]);
 	});
-
-	$scope.fd = {};           // form data from the view
-	$scope.location = $location.path();
-
-
-	// Log out user
-	$scope.logout = function() {
-		sessionFactory.logout();
-		$scope.user = null;
-		sessionFactory.user = null;
-		$location.path('/login');
-	};
-
-	}]);
-});
