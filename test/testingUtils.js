dff9ebc4e36bcec4d4dbd8544ed9e3a71796b5b0
Nolan <njpiland@ncsu.edu>
2015-03-24 23:42:44 -0400

    mongoose load test data

--- test/testingUtils.js
+++ test/testingUtils.js
@@ -11,84 +11,87 @@ var userData = require('./data/users.json');
 
 mongoose.connect('mongodb://' + config.server.host + '/flexibox');
 
-var exports = {
-    dropCollections : function(callback){
-        async.waterfall([
-            function(callback){console.log("removing projects");Projects.remove({}, callback)},
-            function(err, callback){console.log("removing users"); Users.remove({}, callback)}
-        ], callback);
-    },
-    addCollections : function( callback ){
-        var json2Mongoose = function(obj){
-            if(typeof obj === 'object' && obj.hasOwnProperty("_id") ) {
-                obj._id = mongoose.Types.ObjectId(obj['$oid']);
-                obj = json2Mongoose(obj);
+var dropCollections = function(callback){
+    async.waterfall([
+        function(callback){console.log("removing projects");Projects.remove({}, callback)},
+        function(err, callback){console.log("removing users"); Users.remove({}, callback)}
+    ], function(){callback(null)});
+};
+
+var addCollection = function( callback ){
+    var json2Mongoose = function(obj, depth){
+        if(typeof obj === 'object' && obj.hasOwnProperty("_id") ) {
+            obj._id = mongoose.Types.ObjectId(obj['$oid']);
+        }
+        for(var e in obj){
+            if(typeof obj[e] === 'object' && obj[e].hasOwnProperty("_id") ) {
+                obj[e] = json2Mongoose(obj[e], depth + 1);
             }
-            for(var e in obj){
-                if(typeof obj[e] === 'object' && obj[e].hasOwnProperty("_id") ) {
-                    obj[e]._id = mongoose.Types.ObjectId(obj[e]['$oid']);
-                    obj[e] = json2Mongoose(obj[e]);
-                }
-            }
-            return obj;
-        };
-        async.waterfall([
-            function(callback){
-                async.map(userData, function(obj, callback){
-                        obj = json2Mongoose(obj);
-                        var proj = new Projects(obj);
-                        proj.save(function(err){
-                            if(err){
-                                callback(err);
-                            } else {
-                                callback(null);
-                            }
-                        });
-                    },
-                    function(err){
+        }
+        return obj;
+    };
+    async.waterfall([
+        function(callback){
+            async.map(userData, function(obj, callback){
+                    obj = json2Mongoose(obj, 0);
+                    var proj = new Projects(obj);
+                    proj.save(function(err){
                         if(err){
-                            throw new Error(err);
+                            callback(err);
                         } else {
                             callback(null);
                         }
-                    })
-            },
-            function(callback){
-                async.map(projectsData, function(obj, callback){
-                        obj = json2Mongoose(obj);
-                        var user = new Users(obj);
-                        user.save(function(err){
-                            if(err){
-                                callback(err);
-                            } else {
-                                callback(null);
-                            }
-                        });
-                    },
-                    function(err){
+                    });
+                },
+                function(err){
+                    if(err){
+                        throw new Error(err);
+                    } else {
+                        callback(null);
+                    }
+                })
+        },
+        function(callback){
+            async.map(projectsData, function(obj, callback){
+                    obj = json2Mongoose(obj);
+                    var user = new Users(obj);
+                    user.save(function(err){
                         if(err){
-                            throw new Error(err);
+                            callback(err);
                         } else {
                             callback(null);
                         }
-                    })
-            }
-        ], function(err){
-            if(err){
-                throw new Error("Error loading data!!!");
-            }
+                    });
+                },
+                function(err){
+                    if(err){
+                        throw new Error(err);
+                    } else {
+                        callback(null);
+                    }
+                })
+        }
+    ], function(err){
+        if(err){
+            throw new Error("Error loading data!!!");
+        }
+        callback(null);
+    });
+};
+
+module.exports = {
+    dropCollections : dropCollections,
+    addCollections : addCollection,
+    loadTestData : function(callback){
+        async.waterfall([
+            dropCollections,
+            addCollection
+        ], function(){
+            //mongoose.disconnect();
+            //mongoose.connection.close();
             callback();
         })
     },
     URL : "http://" + config.server.host + ":" + config.server.port + "/"
 
 };
-
-console.log(userData);
-
-exports.addCollections(function(err){
-    console.log('dropped collections');
-    mongoose.disconnect();
-    mongoose.connection.close();
-});
-//exports.addCollections();
