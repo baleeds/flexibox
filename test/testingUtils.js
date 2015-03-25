aa3bcdb4291b224f6fe8ccb0abeae8f40da63ead
Nolan <njpiland@ncsu.edu>
2015-03-24 23:15:26 -0400

    Trying to get testing to work

--- test/testingUtils.js
+++ test/testingUtils.js
@@ -1,16 +1,94 @@
 var exec = require('child_process').exec;
+var async = require('async');
+var mongoose = require('mongoose');
+
 var config = require('../config');
+var Users = require('../app/common/models/users');
+var Projects = require('../app/common/models/projects');
 
+var projectsData = require('./data/projects.json');
+var userData = require('./data/users.json');
 
-module.exports = {
-    loadTestData : function(){
-        exec("mongoimport -d flexibox -c users test/data/users.json --drop");
-        exec("mongoimport -d flexibox -c projects test/data/projects.json --drop");
-        var now = Date.now();
-        while(Date.now() - now < 5000){
-            //Wait for 5 seconds so that child processes can finish.
-        }
+mongoose.connect('mongodb://' + config.server.host + '/flexibox');
+
+var exports = {
+    dropCollections : function(callback){
+        async.waterfall([
+            function(callback){console.log("removing projects");Projects.remove({}, callback)},
+            function(err, callback){console.log("removing users"); Users.remove({}, callback)}
+        ], callback);
+    },
+    addCollections : function( callback ){
+        var json2Mongoose = function(obj){
+            if(typeof obj === 'object' && obj.hasOwnProperty("_id") ) {
+                obj._id = mongoose.Types.ObjectId(obj['$oid']);
+                obj = json2Mongoose(obj);
+            }
+            for(var e in obj){
+                if(typeof obj[e] === 'object' && obj[e].hasOwnProperty("_id") ) {
+                    obj[e]._id = mongoose.Types.ObjectId(obj[e]['$oid']);
+                    obj[e] = json2Mongoose(obj[e]);
+                }
+            }
+            return obj;
+        };
+        async.waterfall([
+            function(callback){
+                async.map(userData, function(obj, callback){
+                        obj = json2Mongoose(obj);
+                        var proj = new Projects(obj);
+                        proj.save(function(err){
+                            if(err){
+                                callback(err);
+                            } else {
+                                callback(null);
+                            }
+                        });
+                    },
+                    function(err){
+                        if(err){
+                            throw new Error(err);
+                        } else {
+                            callback(null);
+                        }
+                    })
+            },
+            function(callback){
+                async.map(projectsData, function(obj, callback){
+                        obj = json2Mongoose(obj);
+                        var user = new Users(obj);
+                        user.save(function(err){
+                            if(err){
+                                callback(err);
+                            } else {
+                                callback(null);
+                            }
+                        });
+                    },
+                    function(err){
+                        if(err){
+                            throw new Error(err);
+                        } else {
+                            callback(null);
+                        }
+                    })
+            }
+        ], function(err){
+            if(err){
+                throw new Error("Error loading data!!!");
+            }
+            callback();
+        })
     },
     URL : "http://" + config.server.host + ":" + config.server.port + "/"
 
 };
+
+console.log(userData);
+
+exports.addCollections(function(err){
+    console.log('dropped collections');
+    mongoose.disconnect();
+    mongoose.connection.close();
+});
+//exports.addCollections();
